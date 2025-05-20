d187010a5ab3c374ae4ff7b71e7492fdb9f7be8a
CodingforMegan <79181396+CodingforMegan@users.noreply.github.com>
2025-05-19 21:47:36 -0700

    Update traffic_simulation.py

--- traffic_simulation.py
+++ traffic_simulation.py
@@ -4,3 +4,15 @@
 
 #Course: Spr25_CS_034 CRN 39575
 #----------------------------------------------
+def adjust_for_traffic(graph, time_of_day):
+    traffic_levels = {
+        "morning": "heavy",
+        "afternoon": "moderate",
+        "evening": "light",
+        "night": "light"
+    }
+    new_level = traffic_levels.get(time_of_day, "moderate")
+
+    for u in graph.get_nodes():
+        for edge in graph.neighbors(u):
+            edge.traffic = new_level
